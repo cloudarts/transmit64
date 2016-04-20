5c2e9d788fbf64557d2d3e9c89911a10f35ee95a
Wojciech A. Jurczyk <wajurczyk@gmail.com>
2016-04-20 23:00:44 +0200

    file is being written byte by byte. plus console output

--- pc/transmit64.cpp
+++ pc/transmit64.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -7,33 +9,61 @@ void printUsage(char* progName)
 {
 	cout << endl;
 	cout << "Usage: " << endl;
-	cout <<  progName << " <fileToTransmit> </dev/yourSerialDevice>";
+	cout <<  progName << " <fileToTransmit> </dev/yourSerialDevice> <delayPerByte>";
 	cout << endl;
 	cout << endl;
+}
+
+void printCharWithBinary(const char& c)
+{
+	cout << "\"" << c << "\"" << ": ";
+	for(int i = 7; i >= 0; --i)
+	{
+		cout << ((c >> i) & 1);
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	cout << endl << "Transmit64 Copyright 2016 W.A.Jurczyk" << endl << endl;
 
-	if( argc != 3 )
+	if( argc != 4 )
 	{
 		printUsage(argv[0]);
 		return -1;
 	}
 
-	ifstream ifs(argv[1], ios_base::in | ios_base::binary);
+	ifstream ifs(argv[1], ios_base::in | ios_base::binary | ios_base::ate);
 
 	if( !ifs )
 	{
-		cerr << "Error reading file " << argv[1] << endl;
+		cerr << "Error opening file " << argv[1] << endl;
 		return -1;
 	}
 
+	int filesize = ifs.tellg();
+	ifs.seekg (0, ifs.beg);
+
+	ofstream ofs(argv[2], ios_base::out | ios_base::binary);
+	if( !ofs )
+	{
+		cerr << "Error opening device " << argv[2] << endl;
+		return -1;
+	}
+
+	int delay = atoi( argv[3] );
+
 	char currentChar;
+	int currentPos = 0;
 	while (ifs.read(&currentChar, 1) )
 	{
-		cout << currentChar;	
+		ofs << currentChar;
+		//cout << "\r";
+		printCharWithBinary(currentChar);
+		cout << "; written " << currentPos << "/" << filesize << endl;
+		
+		currentPos++;
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 	}
 
 	cout << endl;
@@ -46,5 +76,9 @@ int main(int argc, char* argv[])
 	{
 		cerr << "Error reading file." << endl;
 	}
+
+	ifs.close();
+	ofs.close();
+
 	return 0;
 }
